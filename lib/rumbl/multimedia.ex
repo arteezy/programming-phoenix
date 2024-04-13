9c9333e6f1141bc774a7d78284469f70a8901f35
Arthur L. <mardybm@gmail.com>
2024-04-13 19:38:34 +0600

    add DB seeding

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -3,6 +3,7 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Repo
   alias Rumbl.Accounts
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Category
 
   def list_videos do
     Repo.all(Video)
@@ -41,6 +42,10 @@ defmodule Rumbl.Multimedia do
     Video
     |> user_videos_query(user)
     |> Repo.get!(id)
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
   end
 
   defp user_videos_query(query, %Accounts.User{id: user_id}) do
